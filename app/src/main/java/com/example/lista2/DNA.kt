0cc8469f6c62366039d355ab113389f7e971fdd5
Hanna Górecka <128626696+haniagorecka@users.noreply.github.com>
2024-04-26 23:51:04 +0200

    3rd commit

--- app/src/main/java/com/example/lista2/DNA.kt
+++ app/src/main/java/com/example/lista2/DNA.kt
@@ -1,11 +1,67 @@
 package com.example.lista2
 
-class DNASequence (var identifier: String, var data: String, var length: Int, val VALID_CHARS: List<Char>)
+class DNASequence (var identifier: String, var data: String, var length: Int)
 {
+val VALID_CHARS: List<Char> = listOf('A', 'C', 'T', 'G')
+
+
+    fun mutate(position: Int, value: Char)
+    {
+        check(this.data.isNotBlank()){"Nić kodująca jest pusta"}
+        check(this.length>position){"Nie istnieje taka pozycja"}
+        check(VALID_CHARS.contains(value.uppercaseChar())){"Niedozwolona wartosc"}
+
+        val str =  StringBuilder(this.data)
+        str.setCharAt(position, value.uppercaseChar())
+        this.data=str.toString()
+
+    }
+    fun complement(): String
+    {
+        check(this.data.isNotBlank()){"Nić kodująca jest pusta"}
+        var complementary= ""
+        for(el in this.data.uppercase())
+        {
+            complementary += when(el) {
+                'A' -> 'T'
+                'C' -> 'G'
+                'T' -> 'A'
+                'G' -> 'C'
+                else -> throw IllegalArgumentException(" Niewłaściwy symbol w nici kodującej")
+
+            }
+
+        }
+        return complementary.reversed()
+    }
+
+    fun transcribe (): RNASequence
+    {
+        check(this.data.isNotBlank()){"Nić matrycowa jest pusta"}
+        var transcibed= ""
+        for(el in this.data.uppercase())
+        {
+            transcibed += when(el) {
+                'A' -> 'U'
+                'C' -> 'G'
+                'T' -> 'A'
+                'G' -> 'C'
+                else -> throw IllegalArgumentException(" Niewłaściwy symbol w nici matrycowej")
+
+            }
+
+        }
+        var RNA = RNASequence(this.identifier, transcibed.reversed(), this.length)
+        return RNA
+    }
+}
+
+class RNASequence (var identifier: String, var data: String, var length: Int)
+{
+    val VALID_CHARS: List<Char> = listOf('A', 'C', 'U', 'G')
 
 
 }
-
 
 
 fun main()
