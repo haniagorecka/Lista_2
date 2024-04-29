ba4587c36655d32146d2767e23f88b861a9d1a21
Hanna Górecka <128626696+haniagorecka@users.noreply.github.com>
2024-04-29 15:23:35 +0200

    7th commit

---
+++ app/src/test/java/com/example/lista2/DNASequenceTest.kt
@@ -0,0 +1,26 @@
+package com.example.lista2
+
+import org.junit.jupiter.api.Assertions.*
+
+class DNASequenceTest {
+
+    @org.junit.jupiter.api.Test
+    fun testToString() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun mutate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun complement() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun transcribe() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun findMotif() {
+    }
+}
